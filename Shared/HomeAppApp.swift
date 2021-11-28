0a4937644eb9779e79587b2f1140ecc7e0b3ca72
Eng Angelo Saber <74116325+AngeloXcode@users.noreply.github.com>
2021-11-28 10:59:43 +0300

    Initial Commit

---
+++ Shared/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  Shared
+//
+//  Created by Angelo Essam on 28/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
